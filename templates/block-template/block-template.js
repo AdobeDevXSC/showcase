8be7afe83eae43adaa088f706b88e61249bfca77
timwlee <timothyl@adobe.com>
2024-10-28 16:48:47 -0700

    Add styling for basic layout

--- templates/block-template/block-template.js
+++ templates/block-template/block-template.js
@@ -1,3 +1,15 @@
 export default function decorate(block) {
-  console.log("block template: ", block);
+  const sections = block.querySelectorAll('.section');
+  const mainWrapper = document.createElement('div');
+
+  mainWrapper.classList.add('main-wrapper');
+
+  sections.forEach((section) => {
+    if (!section.classList.contains('nav')) {
+      mainWrapper.appendChild(section);
+      section.style.display = 'block';
+    }
+  });
+
+  block.appendChild(mainWrapper);
 }
